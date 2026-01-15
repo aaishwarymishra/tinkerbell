e1e8fac9c051f0f03917c93aafe460c3cb008ada
blanky <aaishwarymishra@gmail.com>
2026-01-15 20:26:10 +0530

    added simple linear regression model

---
+++ simple_linear_regression.py
@@ -0,0 +1,58 @@
+import numpy as np
+from tqdm.auto import tqdm
+
+class SimpleLinearRegression:
+    def __init__(self):
+        self.weight: np.ndarray = None
+        self.bias = None
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        assert self.weight is not None and self.bias is not None, "Model is not trained yet."
+        assert x.shape[1] == self.weight.shape[0], "Input features do not match weight dimensions."
+        return np.dot(x, self.weight) + self.bias
+
+    def mse_loss(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        return np.mean((y_true - y_pred) ** 2)
+
+    def backward(self,x: np.ndarray, y_true: np.ndarray, y_pred: np.ndarray) -> tuple[np.ndarray, float]:
+        loss = self.mse_loss(y_true, y_pred)
+        n_samples = x.shape[0]
+
+        dloss_dy_pred = -2 * (y_true - y_pred) / n_samples
+        dy_pred_dbias_sum = np.ones_like(y_true)
+        dy_pred_dweight = np.transpose(x)
+        dloss_dbias = np.ones_like(y_true)
+
+        weights_grad = np.matmul(dy_pred_dweight, (dloss_dy_pred* dy_pred_dbias_sum))
+        bias_grad = np.sum(dloss_dy_pred * dloss_dbias)
+        return weights_grad, bias_grad
+
+    def fit(self, x: np.ndarray, y: np.ndarray, learning_rate: float = 0.01, epochs: int = 1000):
+        assert x.shape[0] == y.shape[0], "Number of samples in x and y must be the same."
+        n_samples, n_features = x.shape
+        self.weight = np.random.rand(n_features, 1)
+        self.bias = np.random.rand(1,)
+
+        for epoch in tqdm(range(epochs)):
+            y_pred = self.forward(x)
+            error = self.mse_loss(y, y_pred)
+            w_grad, b_grad = self.backward(x, y, y_pred)
+
+            self.weight -= learning_rate * w_grad
+            self.bias -= learning_rate * b_grad
+            if epoch % 100 == 0:
+                print(f"Epoch {epoch}/{epochs}, Loss: {error:.4f}")
+
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        return np.dot(x, self.weight) + self.bias
+
+if __name__ == "__main__":
+    # Example usage
+    x = np.random.random((100, 3))
+    y = np.matmul(x, np.array([[2.0], [3.0], [4.0]])) + 5.0 
+
+    model = SimpleLinearRegression()
+    model.fit(x, y, learning_rate=0.01, epochs=1000)
+
+    print("Trained weights:", model.weight)
+    print("Trained bias:", model.bias)

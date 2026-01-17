d6c5c9d4709fadb12dbdd4d46d6afbc54554ad24
blanky <aaishwarymishra@gmail.com>
2026-01-17 15:01:01 +0530

    implement batch processing and improve gradient calculations in SimpleLinearRegression

---
+++ blocks.py
@@ -0,0 +1,93 @@
+from matplotlib.tests.test_widgets import ax
+import numpy as np
+
+class FunctionBlock:
+    def __init__(self):
+        pass
+
+    def __call__(self, input):
+        self._input = input
+        self._output = self.forward(input)
+        return self._output
+
+    def forward(self, input):
+        raise NotImplementedError("Forward method not implemented.")
+
+    def backward(self, grad_output):
+        self.assert_same_shape(grad_output, self._output)
+        self._grad_input = self.input_grad(grad_output)
+        self.assert_same_shape(self._grad_input, self._input)
+        return self._grad_input
+        
+
+    def input_grad(self, grad_output):
+        raise NotImplementedError("Backward method not implemented.")
+
+    def assert_same_shape(self, a, b):
+        assert a.shape == b.shape, f"Shape mismatch: {a.shape} vs {b.shape}" 
+
+
+class LinearBlock(FunctionBlock):
+    def __init__(self, param):
+        super().__init__()
+        self.param = param
+
+    def backward(self, grad_output):
+        self.assert_same_shape(grad_output, self._output)
+        self._grad_input = self.input_grad(grad_output)
+        self.assert_same_shape(self._grad_input, self._input)
+        self._grad_param = self.param_grad(grad_output)
+        self.assert_same_shape(self._grad_param, self.param)
+        return self._grad_input
+
+    def param_grad(self, grad_output):
+        raise NotImplementedError("Parameter gradient method not implemented.")
+
+
+class WeightMatrixBlock(LinearBlock):
+    def __init__(self, input_dim: int, weight: np.ndarray):
+        super().__init__(weight)
+        
+    def forward(self, input: np.ndarray) -> np.ndarray:
+        return np.matmul(input, self.param)
+
+    def input_grad(self, grad_output: np.ndarray) -> np.ndarray:
+        if self.param.ndim > 2:
+            axis = list(range(grad_output.ndim - 2))
+            axis.extend([grad_output.ndim - 1, grad_output.ndim - 2])
+        else:
+            axis = [1, 0]
+        return np.matmul(grad_output, np.transpose(self.param,tuple(axis)))
+
+    def param_grad(self, grad_output: np.ndarray) -> np.ndarray:
+        if self._input.ndim > 2:
+            _in = np.reshape(self._input, (-1, self._input.shape[-1]))
+            grad_out = np.reshape(grad_output, (-1, grad_output.shape[-1]))
+            return np.matmul(np.transpose(_in), grad_out)    
+
+        return np.matmul(np.transpose(self._input), grad_output)
+
+class BiasBlock(LinearBlock):
+    def __init__(self, bias: np.ndarray):
+        super().__init__(bias)
+
+    def forward(self, input: np.ndarray) -> np.ndarray:
+        return input + self.param
+
+    def input_grad(self, grad_output: np.ndarray) -> np.ndarray:
+        return grad_output
+
+    def param_grad(self, grad_output: np.ndarray) -> np.ndarray:
+        return np.sum(grad_output, axis=0, keepdims=True)
+
+
+class SigmoidBlock(FunctionBlock):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input: np.ndarray) -> np.ndarray:
+        return 1 / (1 + np.exp(-input))
+
+    def input_grad(self, grad_output: np.ndarray) -> np.ndarray:
+        sigmoid_derivative = self._output * (1 - self._output)
+        return grad_output * sigmoid_derivative
--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -1,4 +1,6 @@
+from matplotlib.pylab import permutation
 from fontTools.afmLib import error
+import math
 import numpy as np
 from tqdm.auto import tqdm
 
@@ -21,17 +23,17 @@ class SimpleLinearRegression:
         if loss == 'mse':
             dloss_dpred = -2 * (y_true - y_pred) / n_samples
         elif loss == 'rmse':
-            rmse = self.rmse_loss(y_true, y_pred)
+            rmse = self.rmse_loss(y_true, y_pred) + 1e-8  # to avoid division by zero
             dloss_dpred = - (y_true - y_pred) / (rmse * n_samples)
-        dpred_dN = np.ones_like(y_true)
-        dN_dweight = np.transpose(x)
+        dpred_dM = np.ones_like(y_true)
+        dM_dweight = np.transpose(x)
         dpred_dbias = np.ones_like(y_true)
 
-        weights_grad = np.matmul(dN_dweight, (dloss_dpred * dpred_dN))
+        weights_grad = np.matmul(dM_dweight, (dloss_dpred * dpred_dM))
         bias_grad = np.sum(dloss_dpred * dpred_dbias)
         return weights_grad, bias_grad
 
-    def fit(self, x: np.ndarray, y: np.ndarray, learning_rate: float = 0.01, epochs: int = 1000,loss='mse'):
+    def fit(self, x: np.ndarray, y: np.ndarray, learning_rate: float = 0.01, epochs: int = 1000,loss='mse', batch_size: int = 32):
         assert x.shape[0] == y.shape[0], "Number of samples in x and y must be the same."
         n_samples, n_features = x.shape
         self.weight = np.random.rand(n_features, 1)
@@ -41,17 +43,29 @@ class SimpleLinearRegression:
         elif loss == 'rmse':
             loss_function = self.rmse_loss   
 
+        batches = math.ceil(n_samples / batch_size)
+
         for epoch in tqdm(range(epochs)):
-            y_pred = self.forward(x)
-            w_grad, b_grad = self.backward(x, y, y_pred,loss=loss)
-            error   = loss_function(y, y_pred)
-            self.weight -= learning_rate * w_grad
-            self.bias -= learning_rate * b_grad
+            avg_loss = 0
+            permutation = np.random.permutation(n_samples)
+            for x_batch, y_batch in self.get_batch(x, y, batch_size,permutation):
+                y_pred = self.forward(x_batch)
+                w_grad, b_grad = self.backward(x_batch, y_batch, y_pred,loss=loss)
+                avg_loss += loss_function(y_batch, y_pred)
+                self.weight -= learning_rate * w_grad
+                self.bias -= learning_rate * b_grad
             if epoch % 100 == 0:
-                print(f"Epoch {epoch}/{epochs}, Loss: {error:.4f}")
+                print(f"Epoch {epoch}/{epochs}, Loss: {(avg_loss/batches):.4f}")
 
     def predict(self, x: np.ndarray) -> np.ndarray:
         return np.dot(x, self.weight) + self.bias
+
+    def get_batch(self, x: np.ndarray, y: np.ndarray, batch_size: int,permutation:list[int]):
+        n_samples = x.shape[0]
+        for i in range(0, n_samples, batch_size):
+            x_batch = x[permutation[i:i + batch_size]]
+            y_batch = y[permutation[i:i + batch_size]]
+            yield x_batch, y_batch
     
     def rmse_loss(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
         return np.sqrt(np.mean((y_true - y_pred) ** 2))
@@ -66,9 +80,14 @@ if __name__ == "__main__":
     x = np.random.random((100, 3))
     y = np.matmul(x, np.array([[2.0], [3.0], [4.0]])) + 5.0 
 
+    print("Input shape:", x.shape)
+    print("Output shape:", y.shape)
+
     model = SimpleLinearRegression()
     model.fit(x, y, learning_rate=0.01, epochs=1000,loss="rmse")
 
     print("Trained weights:", model.weight)
     print("Trained bias:", model.bias)
     print("R-squared:", model.r_squared(y, model.predict(x)))
+    print("RMSE:", model.rmse_loss(y, model.predict(x)))
+    print("MSE:", model.mse_loss(y, model.predict(x)))
